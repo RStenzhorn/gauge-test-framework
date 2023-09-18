5c9a1e6610b2beb2c8f0b0ac15630c8b417d8228
RStenzhorn <robert.stenzhorn@outlook.de>
2023-09-18 21:44:26 +0200

    init

--- specs/test.spec
+++ specs/test.spec
@@ -3,7 +3,7 @@ Created by Robert on 15.09.2023
 
 
 ### Scenario Heading 1
-* Get all players "1020"
+* Get all players "1025"
 
 ## Scenario Heading 2
 * Get all players "1025"
--- src/main/java/de/rjst/gaugetestframework/logic/Player.java
+++ src/main/java/de/rjst/gaugetestframework/logic/Player.java
@@ -2,11 +2,14 @@ package de.rjst.gaugetestframework.logic;
 
 import lombok.Data;
 
+import java.time.LocalDate;
+
 @Data
 public class Player {
+    private Long id;
+    private LocalDate birthDay;
     private String firstName;
     private String password;
     private String gender;
     private String surname;
-    private Integer id;
 }
